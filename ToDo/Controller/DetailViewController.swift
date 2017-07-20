73d7ba4023a8f7066d3bc601ab7e234bad8ac446
Leonardo Vinicius Kaminski Ferreira <leonardo@mindvalley.com>
2017-07-20 11:45:28 +0800

    Adds DetailViewController implementation and tests

---
+++ ToDo/Controller/DetailViewController.swift
@@ -0,0 +1,59 @@
+//
+//  DetailViewController.swift
+//  ToDo
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 20/07/17.
+//  Copyright © 2017 iOS Wizards. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var placeLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var mapView: MKMapView!
+    
+    var itemInfo: (ItemManager, Int)?
+    
+    let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter
+    }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let itemInfo = itemInfo else {
+            return
+        }
+        let item = itemInfo.0.item(at: itemInfo.1)
+        titleLabel.text = item.title
+        placeLabel.text = item.location?.name
+        descriptionLabel.text = item.itemDescription
+        
+        if let timestamp = item.timestamp {
+            let date = Date(timeIntervalSince1970: timestamp)
+            dateLabel.text = dateFormatter.string(from: date)
+        }
+        
+        if let coordinate = item.location?.coordinate {
+            let region = MKCoordinateRegionMakeWithDistance(coordinate, 100, 100)
+            
+            mapView.region = region
+        }
+        
+    }
+    
+    func checkItem() {
+        if var itemInfoI = itemInfo {
+            itemInfoI.0.checkItem(at: itemInfoI.1)
+            itemInfo = itemInfoI
+        }
+    }
+
+}
